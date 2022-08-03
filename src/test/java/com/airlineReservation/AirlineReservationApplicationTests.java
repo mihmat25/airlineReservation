dee5822689c069ba39647c12f91c8488acc406c9
Mihály Mátyás <mihamatyas@gmail.com>
2022-08-03 12:45:31 +0300

    Initial commit

---
+++ src/test/java/com/airlineReservation/AirlineReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlineReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
